c41216c7ab17cd7a5f2d80016c96c229b16471b0
hanifek <hanifekurnaz@outlook.com>
2020-09-26 00:57:50 -0700

    first commit

---
+++ LandmarkBook/imageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  imageViewController.swift
+//  LandmarkBook
+//
+//  Created by hks on 26.09.2020.
+//
+
+import UIKit
+
+class imageViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var landmarkLabel: UILabel!
+    var selectedLandmarkName = ""
+    var selectedLandmarkImage = UIImage()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        landmarkLabel.text = selectedLandmarkName
+        imageView.image = selectedLandmarkImage
+    }
+   
+
+}
